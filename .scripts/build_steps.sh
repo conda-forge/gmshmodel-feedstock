95c375b4a741f1dc40639134a142d2518ed25c51
Philipp Metsch <philipp.metsch@tu-dresden.de>
2024-03-28 08:53:11 +0100

    MNT: Re-rendered with conda-build 24.3.0, conda-smithy 3.34.0, and conda-forge-pinning 2024.03.27.17.58.50

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -43,14 +43,6 @@ setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 source run_conda_forge_build_setup
 
-
-# Install the yum requirements defined canonically in the
-# "recipe/yum_requirements.txt" file. After updating that file,
-# run "conda smithy rerender" and this line will be updated
-# automatically.
-/usr/bin/sudo -n yum install -y mesa-libGLU mesa-libgl mesa-libGL libglu1-mesa
-
-
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
